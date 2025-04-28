e66705a54fdcdee9fb46724709cf88ae1ce884b2
ThapeloMasasa <thapelomasasa5@gmail.com>
2025-04-28 13:54:29 -0400

    files

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+export default function index() {
+  return (
+    <View className="bg-red-200 pt-20">
+      <Text className="text-3xl text-center">Masasa</Text>
+    </View>
+  )
+}
