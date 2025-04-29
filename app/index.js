afee0dcba69d55f3405234aeacc7ab0373efea11
ThapeloMasasa <thapelomasasa5@gmail.com>
2025-04-29 19:07:21 -0400

    log in state

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import { View, ActivityIndicator } from 'react-native'
 import React from 'react'
 
 
-export default function startPage() {
+export default function () {
   return (
     <View className="flex-1 justify-center">
       <ActivityIndicator size="large" color="gray" />
--- app/signin.js
+++ app/signin.js
@@ -1,23 +1,22 @@
-import { View, Text , Image,TextInput, TouchableOpacity, Pressable } from 'react-native'
+import { View, Text , Image,TextInput, TouchableOpacity, Pressable, Alert,ActivityIndicator } from 'react-native'
 import React, { useRef, useState } from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { StatusBar } from 'expo-status-bar'
 import { Octicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
-import Loading from '../components/Loading';
-
 export default function SignIn() {
 
     const router = useRouter();
-    const passwodRef = useRef("");
+    const passwordRef = useRef("");
     const emailRef = useRef("");
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false)
 
     const handleLogin = async ()=>{
-        if(!emailRef.current || !passwodRef.current){
-            Alert.alert("Sign In", "Please fill all the fields!");
+        if(!emailRef.current || !passwordRef.current){
+            Alert.alert("Please fill both the password and email.");
             return;
         }
+        setLoading(true);
     }
   return (
     <View className="flex-1">
@@ -45,40 +44,44 @@ export default function SignIn() {
                 <View style={{height:hp(7)}} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-2xl ">
                     <Octicons name="lock" size={hp(2.7)} color="gray"/>
                     <TextInput 
-                        onChangeText={value=> passwodRef.value= value}
-                        style={{fontSize: hp(2)}}
-                        className="flex-1 font-semibold text-neutral-700"
-                        placeholder="Password"
-                        secureTextEntry
-                        placeholderTextColor={'gray'}
-                    />
+                                onChangeText={value => passwordRef.current= value}
+                                style={{ fontSize: hp(2) }}
+                                className="flex-1 font-semibold text-neutral-700"
+                                placeholder="Password"
+                                secureTextEntry
+                                placeholderTextColor={'gray'}
+/>
+
                 </View>
                 <Text style={{fontSize:hp(1.8)}} className="font-semibold text-right text-neutral-500" >Forgot password?</Text>
 
                 </View>
 
                 {/*submit button */}
+
                 <View>
                     {
-                        loading? (
-                            <View className="flex-row justify-center">
-                                <Loading size={hp(8)} />
-                            </View>
-                        ):
-                        (<TouchableOpacity onPress={handleLogin} style={{height: hp(6.5)}}className="bg-indigo-500 rounded-xl justify-center items-center">
-                        <Text style={{fontSize: hp(2.7)}} className="text-white font-bold tracking-wider">
-                            Sign In
-                        </Text>
-                    </TouchableOpacity>)
+                        loading ?(
+                                 <View className="flex-1 justify-center pb-10">
+                                  <ActivityIndicator size="large" color="gray" />
+                                </View>
+                        ):(
+                            <TouchableOpacity onPress={handleLogin} style={{height: hp(6.5)}}className="bg-indigo-500 rounded-xl justify-center items-center">
+                            <Text style={{fontSize: hp(2.7)}} className="text-white font-bold tracking-wider">
+                                Sign In
+                            </Text>
+                        </TouchableOpacity>
+                        )
                     }
                 </View>
+              
 
                 {/*sign up text*/}
 
                 <View className="flex-row justify-center">
                     <Text style={{fontSize: hp(1.8)}} className="font-semibold text-neutral-500">Don't have an Account? </Text>
                     <Pressable onPress={()=>router.push('signup')}>
-                    <Text style={{fontSize: hp(1.8)}}className="font-bold text-neutral-500 text-indigo-500">Sign Up</Text>
+                    <Text style={{fontSize: hp(1.8)}}className="font-bold text-indigo-500">Sign Up</Text>
                     </Pressable>
                    
                 </View>
